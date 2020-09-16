50ef2ebab232d613c231f712a0ac18cb1ca7a21f
David Laeremans <morej21@hotmail.com>
2020-09-16 07:16:08 +0200

    Columnset.AddColumn; ColumnSet.AddColumns

--- rd-xrmquery/src/xrmquery/XrmColumnSet.ts
+++ rd-xrmquery/src/xrmquery/XrmColumnSet.ts
@@ -2,16 +2,28 @@ import { XrmColumn } from "./XrmColumn";
 
 export class XrmColumnSet {
     AllColumns: boolean = false;
-
+    public Columns: XrmColumn[];
+    
     constructor(allColumns?: boolean) {
         if (allColumns)
             this.AllColumns = allColumns
         this.Columns = [];
     }
+
     static FromColumns(...columns: string[]): XrmColumnSet {
         let returnColumnSet = new XrmColumnSet(false);
         returnColumnSet.Columns.push(...columns.map(c => new XrmColumn(c)));       
         return returnColumnSet
+    }    
+
+    public AddColumn(column: string){
+        this.AllColumns = false;
+        this.Columns.push(new XrmColumn(column));
     }
-    public Columns: XrmColumn[];
+
+    public AddColumns(...columns: string[]){
+        this.AllColumns = false;
+        this.Columns.push(...columns.map(c => new XrmColumn(c)));
+    }
+
 }
